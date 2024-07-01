b2aa11ad027f38f488eaf785447afe50d9a3e84e
pgrigolli <pedrogrigolli30@gmail.com>
2024-07-01 15:25:17 -0300

    aula77/81

---
+++ aula77/main.js
@@ -0,0 +1,60 @@
+function Calculadora(){
+    this.display = document.querySelector('.display');
+    
+    this.inicia = () => {
+        this.capturaCliques();
+        this.capturaEnter();
+    }
+
+    this.capturaEnter = () => {
+        document.addEventListener('keypress', e =>{
+            if(e.key !== "Enter") return;
+            this.equal();
+        })
+    }
+    
+    this.capturaCliques = () => {
+        document.addEventListener('click', event => {
+            const el = event.target;
+            if(el.classList.contains('btn-num')) this.addNumDisplay(el);
+            if(el.classList.contains('btn-clear')) this.clear();
+            if(el.classList.contains('btn-del')) this.del();
+            if(el.classList.contains('btn-eq')) this.equal(el);
+
+            this.display.focus();
+        });
+    };
+
+    this.addNumDisplay = el => this.display.value += el.innerText;
+
+    this.clear = () => this.display.value = '';
+
+    this.del = () => this.display.value = this.display.value.slice(0, -1);
+
+    this.equal = () => {
+        try {
+        const conta = eval(this.display.value);
+
+        if(!conta){
+            alert('Conta invalida');
+
+        }
+
+        this.display.value = conta;
+
+        }catch(e){
+            alert('Conta Invalida');
+            return;
+        }
+    }
+
+
+   
+
+
+}
+
+
+
+const calculadora = new Calculadora();
+calculadora.inicia();
---
+++ aula81/main.js
@@ -0,0 +1,4 @@
+const nomes = ['Pedro', 'Julia', 'Eduardo'];
+
+delete nomes[1];
+console.log(nomes);
